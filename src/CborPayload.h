1f7e243f4f59cbc761a2c5587fdfdc79c6deefbb
Epiller <epiller@users.noreply.github.com>
2019-11-04 11:20:28 +0100

    Updated CBOR Payload capacity

--- src/CborPayload.h
+++ src/CborPayload.h
@@ -10,7 +10,7 @@
 
 class CborPayload : public Payload {
 public:
-    CborPayload(unsigned int capacity = 251);
+    CborPayload(unsigned int capacity = 256);
     ~CborPayload();
 
     template<typename T> bool set(char *assetName, T value);
